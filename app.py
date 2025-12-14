920243848e40ce4ecaffa2f213926d22e0fd4202
Muhammad Zubair <120093232+muhammad-zubair17@users.noreply.github.com>
2025-12-14 09:45:35 +0500

    Enable CORS for the Flask app
    Added CORS support to the Flask application.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import numpy as np
 import pickle
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 model = pickle.load(open('car_model.pkl', 'rb'))
 
@@ -28,5 +30,6 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
