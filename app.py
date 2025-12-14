332ecc12b79fd414e3ba26f2ccaed043550d89be
Muhammad Zubair <120093232+muhammad-zubair17@users.noreply.github.com>
2025-12-14 09:11:08 +0500

    Add dynamic port configuration for Flask app
    Updated the app to run on a specified port from environment variables.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -24,5 +25,8 @@ def predict():
     prediction = model.predict(features)[0]
     return jsonify({'success': True, 'prediction': round(prediction, 2)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
